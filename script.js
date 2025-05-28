73b5f32c940ab0357ceb1c33d81758773998ed77
Dubem Studio <info.dubemstudio@gmail.com>
2025-05-28 11:48:45 +0100

    html css styling done

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 
+function setVH() {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+setVH();
+window.addEventListener('resize', setVH);
 
 const btn = document.querySelector('.btn');
 
